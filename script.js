43f58789a27f3e01d0c04fc452463149eb2d54f7
nathanjh-28 <nathan@nathanjharris.com>
2023-07-14 22:48:52 -0400

    placeholder follows default content

--- script.js
+++ script.js
@@ -3,9 +3,17 @@ const data = {
     "h1": ["", "This is the main header"]
 }
 
+function updatePlaceholder() {
+    document.getElementById('header-input').setAttribute('placeholder', data['h1'][1])
+}
+updatePlaceholder()
+
 function updateH1() {
     if (data['h1'][0]) document.getElementById('h1').innerText = data['h1'][0]
-    else document.getElementById('h1').innerText = data['h1'][1];
+    else {
+        document.getElementById('h1').innerText = data['h1'][1];
+        updatePlaceholder()
+    }
 }
 updateH1()
 
