951ba4f16c9522459e5cc5f362eae3a081b52978
nathanjh-28 <nathan@nathanjharris.com>
2023-07-14 22:45:45 -0400

    created a default content for when the form field is empty.

--- script.js
+++ script.js
@@ -1,10 +1,11 @@
 
 const data = {
-    "h1": "This is a header from json"
+    "h1": ["", "This is the main header"]
 }
 
 function updateH1() {
-    document.getElementById('h1').innerText = data['h1'];
+    if (data['h1'][0]) document.getElementById('h1').innerText = data['h1'][0]
+    else document.getElementById('h1').innerText = data['h1'][1];
 }
 updateH1()
 
@@ -12,7 +13,7 @@ const headerInput = document.getElementById('header-input')
 const headerEl = document.getElementById('h1')
 
 headerInput.addEventListener("change", e => {
-    data['h1'] = e.target.value
+    data['h1'][0] = e.target.value
     updateH1()
 
 })
