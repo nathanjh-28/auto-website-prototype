0435da90faebb9c3caef3859638c4ec2ef5b16c4
nathanjh-28 <nathan@nathanjharris.com>
2023-07-14 23:08:04 -0400

    consolidated start functions in to one

--- script.js
+++ script.js
@@ -3,19 +3,24 @@ const data = {
     "h1": ["", "This is the main header"]
 }
 
-function updatePlaceholder() {
+function renderWebPage() {
+    update_h1_Placeholder()
+    updateH1()
+}
+
+renderWebPage()
+
+function update_h1_Placeholder() {
     document.getElementById('header-input').setAttribute('placeholder', data['h1'][1])
 }
-updatePlaceholder()
 
 function updateH1() {
     if (data['h1'][0]) document.getElementById('h1').innerText = data['h1'][0]
     else {
         document.getElementById('h1').innerText = data['h1'][1];
-        updatePlaceholder()
+        update_h1_Placeholder()
     }
 }
-updateH1()
 
 const headerInput = document.getElementById('header-input')
 const headerEl = document.getElementById('h1')
