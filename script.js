fa50edb2373a8bcd7e5430b4f8ab73b2f0723006
nathanjh-28 <nathan@nathanjharris.com>
2023-07-14 22:41:37 -0400

    added a js object to keep track of state while updating the website

--- script.js
+++ script.js
@@ -1,5 +1,18 @@
 
+const data = {
+    "h1": "This is a header from json"
+}
+
+function updateH1() {
+    document.getElementById('h1').innerText = data['h1'];
+}
+updateH1()
+
 const headerInput = document.getElementById('header-input')
 const headerEl = document.getElementById('h1')
 
-headerInput.addEventListener("change", e => headerEl.innerText = e.target.value)
+headerInput.addEventListener("change", e => {
+    data['h1'] = e.target.value
+    updateH1()
+
+})
