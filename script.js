907049212b7b9332b209b1520619ba19bee861d9
nathanjh-28 <nathan@nathanjharris.com>
2023-07-15 21:31:54 -0400

    added a subtitle compenent with input

--- script.js
+++ script.js
@@ -1,10 +1,12 @@
 
 const data = {
-    "h1": ["", "This is the main header"]
+    "h1": ["", "Welcome to this landing page"],
+    "subtitle": ["", "Lorem ipsum dolor sit amet consectetur adipisicing elit. Eum nostrum fugiat dolore, porro amet culpa exercitationem ab quibusdam magnam consequuntur."]
 }
 
 function renderWebPage() {
     update_h1_Placeholder()
+    updateSubtitle()
     updateH1()
 }
 
@@ -22,13 +24,26 @@ function updateH1() {
     }
 }
 
+function updateSubtitle() {
+    if (data['subtitle'][0]) document.getElementById('subtitle').innerText = data['subtitle'][0]
+    else {
+        document.getElementById('subtitle').innerText = data['subtitle'][1];
+        //update_h1_Placeholder()
+    }
+}
+
+
 const headerInput = document.getElementById('header-input')
 const headerEl = document.getElementById('h1')
 
 headerInput.addEventListener("change", e => {
     data['h1'][0] = e.target.value
     updateH1()
+})
 
+document.getElementById('subtitle-input').addEventListener("change", e => {
+    data['subtitle'][0] = e.target.value;
+    updateSubtitle()
 })
 
 document.getElementById('hide-btn').addEventListener('click', () => {
